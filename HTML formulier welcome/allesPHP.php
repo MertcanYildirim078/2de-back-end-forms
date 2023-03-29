b15ad65eafd395b7363f329b652bf93a910f98e5
MertcanYildirim078 <99068314@mydavinci.nl>
2023-03-29 11:48:58 +0200

    Validation require field progress

--- HTML formulier welcome/allesPHP.php
+++ HTML formulier welcome/allesPHP.php
@@ -8,11 +8,30 @@
 </head>
 <body>
     <?php 
+    $name = $email = "";
+    $nameErr = $emailErr = "";
 
-      if ($_SERVER["REQUEST_METHOD"] == "POST") {
-        $name = test_input($_POST["name"]);
-        $email = test_input($_POST["email"]);
-      }
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        if (empty($_POST["name"])) {
+          $nameErr = "Name is required";
+        } 
+        
+            else {
+            $name = test_input($_POST["name"]);
+            }
+      
+        if (empty($_POST["email"])) {
+          $emailErr = "Email is required";
+        } 
+        
+            else {
+            $email = test_input($_POST["email"]);
+            }      }
+
+    //   if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    //     $name = test_input($_POST["name"]);
+    //     $email = test_input($_POST["email"]);
+    //   }
       
       function test_input($data) {
         $data = trim($data);
@@ -20,32 +39,37 @@
         $data = htmlspecialchars($data);
         return $data;
       }
-      ?>
 
+      ?>
     <form id="form" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" method="post" >
         <div id="inputContainerAll">
 
             <div id="naamInputContainer">
                 <input id="naamInput" placeholder="Uw naam" type="text" name="name">
+                <span class="error">* <?php echo $nameErr;?></span>
             </div>
 
             <div id="emailInputContainer">
                 <input id="emailInput" placeholder="Uw email" type="text" name="email">
+                <span class="error">* <?php echo $emailErr;?></span>
             </div>
 
-            <input type="submit" id="submitButton">
+            <input type="submit" id="submitButton" value="Submit">
 
             <?php 
     
-                echo "<p id = 'name'> Welcome {$name} </p> <br>";
+                if ($name != "") {
+                    echo "<p id = 'name'> Welcome {$name} </p> <br>";
+                }
+
+                if ($email != "") {
                 echo "<p id = 'email'> Your email address is: {$email}</p> ";
-                
+                }
+
             ?>
             
         </div>
     </form>
-    
- 
 
 <style>
 
@@ -67,6 +91,10 @@
 
 #name, #email {
     justify-content: center;
+}
+
+.error {
+    color: red;
 }
 </style>
 
